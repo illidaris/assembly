16ad769c3a39865b371b1ea0464f03442f70a4a9
Joseph <ivan_whisper@outlook.com>
2023-09-03 14:01:43 +0800

    add

--- assembly.go
+++ assembly.go
@@ -29,11 +29,30 @@ func Info() map[string]string {
 		"name":         Name,
 		"version":      Version,
 		"comit_id":     CommitID,
+		"comit_time":   CommitTime,
+		"build_time":   time.Unix(buildTime, 0).Format("2006-01-02 15:04:05"),
+		"build_number": BuildNumber,
+		"build_job":    BuildJob,
+		"args":         Args,
+	}
+}
+
+func AllInfo() map[string]string {
+	if len(CommitUnix) > 0 {
+		commitTime, _ := strconv.ParseInt(CommitUnix, 10, 64)
+		CommitTime = time.Unix(commitTime, 0).Format("2006-01-02 15:04:05")
+	}
+	buildTime, _ := strconv.ParseInt(BuildTime, 10, 64)
+	return map[string]string{
+		"name":         Name,
+		"version":      Version,
+		"comit_id":     CommitID,
 		"comit_author": CommitAuthor,
 		"comit_time":   CommitTime,
 		"build_time":   time.Unix(buildTime, 0).Format("2006-01-02 15:04:05"),
 		"build_number": BuildNumber,
 		"build_job":    BuildJob,
 		"args":         Args,
+		"hideArgs":     HideArgs,
 	}
 }
